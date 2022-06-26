ec74cb3a9a4e122230649ea82c837baa078f4b80
Maksat <boktaban@gmail.com>
2022-06-26 19:57:35 +0600

    create Procfile

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -7,7 +7,8 @@ import {UserControllers} from "./UserControllers"
 import {TodoList} from "./TodoList"
 
 export const Todo = () => {
-    const BACKEND_URL = 'http://localhost:5000'
+    // const BACKEND_URL = 'http://localhost:5000'
+    const BACKEND_URL = 'https://pacific-forest-33278.herokuapp.com'
 
     const [todo, setTodo] = useState([])
     const [todoAddInput, setTodoAddInput] = useState('')
