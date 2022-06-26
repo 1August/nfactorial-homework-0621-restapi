9da35fd57224d29747750f36083386201a7a4b81
Maksat <boktaban@gmail.com>
2022-06-26 20:05:37 +0600

    changed port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json({extended: true}))
 
 app.use('/', require('./routes/todo'))
 
-const PORT  = config.get('port') || 5000
+const PORT  = process.env.PORT || 5000
 
 const start = async () => {
     try{
